0310aa86f6d4d051aae1416ad0f8cb840f7fb9b1
Mamun Mohamed <x9767907z@iespoblenou.org>
2020-06-15 20:49:31 +0200

    Implemets Layers close #2

--- src/main/java/com/mamunmohamed/meetup/MeetupApplication.java
+++ src/main/java/com/mamunmohamed/meetup/MeetupApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MeetupApplication {
+public class  MeetupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeetupApplication.class, args);
